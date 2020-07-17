238fd026848daab8fb0d702cc54ddeff9b649e38
Ezequiel Lynch <ezeqlynch@hotmail.com>
2020-07-16 21:49:35 -0300

    fixed cap

--- worker.js
+++ worker.js
@@ -6,7 +6,7 @@ const runWorker = (e) => {
     let s = new NodeSA(e.totalUlt, e.totalIm, e.stage, e.totalPCores, 
         l, m, r, [e.ref, e.shp, e.di, e.reg, e.lec], 0, 0, 
         e.dmgMult, e.hpMult, e.currUlt, e.currIm, null, 
-        e.expedpoints, e.fuelUpgrades, e.totalLab, e.trophies100)
+        e.expedPoints, e.fuelUpgrades, e.totalLab, e.trophies100);
     let root = s.clone();
     root.challengeNumber = "INIT";
     let p = new ArenaProblem(s);
